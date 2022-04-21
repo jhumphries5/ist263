d56e0ef8bfc9656228ec4f39377ae13d45616893
jhumphries5 <58453712+jhumphries5@users.noreply.github.com>
2022-04-21 00:17:35 -0400

    javascript

---
+++ week11/myscript.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Intro to JavaScript Part 2</title>
+        <script>
+        SayHello() {
+
+        }
+        </script>
+
+    </head>
+
+    <body>
+
+    <a onclick="SayHello();" href="">Click Here</a>
+    <!--
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero rem odio impedit, veniam eum ea voluptatem saepe quidem mollitia eligendi, enim a quaerat id doloremque cumque. Cupiditate, laborum esse.</p>
+
+    </body>
+</html>
